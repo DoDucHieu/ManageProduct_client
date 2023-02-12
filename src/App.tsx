a2276203942f2ec97b5f849f424213da132d21a5
DoDucHieu <hieuncncnc@gmail.com>
2023-02-12 17:19:09 +0700

    do duc hieu commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
+import "./asset/style/App.scss";
+import LayoutWrapper from "./component/LayoutWrapper";
+import { configureStore } from "./store/store";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<Provider store={configureStore.store}>
+				<PersistGate
+					loading={null}
+					persistor={configureStore.persistor}
+				>
+					<BrowserRouter>
+						<LayoutWrapper />
+					</BrowserRouter>
+				</PersistGate>
+			</Provider>
+		</>
+	);
 }
 
 export default App;
